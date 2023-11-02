8557475866f7da9e47aa8faa26bb95511305fcf5
Yersa Nordman <yersa@finwo.nl>
2023-11-02 01:00:33 +0100

    Added -lpthread cflag on posix for tinycthread

--- tinycthread/tinycthread/config.mk
+++ tinycthread/tinycthread/config.mk
@@ -1,1 +1,34 @@
 SRC+=__DIRNAME/source/tinycthread.c
+
+ifeq ($(OS),Windows_NT)
+    # CFLAGS += -D WIN32
+    ifeq ($(PROCESSOR_ARCHITEW6432),AMD64)
+        # CFLAGS += -D AMD64
+    else
+        ifeq ($(PROCESSOR_ARCHITECTURE),AMD64)
+            # CFLAGS += -D AMD64
+        endif
+        ifeq ($(PROCESSOR_ARCHITECTURE),x86)
+            # CFLAGS += -D IA32
+        endif
+    endif
+else
+    UNAME_S := $(shell uname -s)
+    override CFLAGS+=-lpthread
+    ifeq ($(UNAME_S),Linux)
+        # CFLAGS += -D LINUX
+    endif
+    ifeq ($(UNAME_S),Darwin)
+        # CFLAGS += -D OSX
+    endif
+    UNAME_P := $(shell uname -p)
+    ifeq ($(UNAME_P),x86_64)
+        # CFLAGS += -D AMD64
+    endif
+    ifneq ($(filter %86,$(UNAME_P)),)
+        # CFLAGS += -D IA32
+    endif
+    ifneq ($(filter arm%,$(UNAME_P)),)
+        # CFLAGS += -D ARM
+    endif
+endif
