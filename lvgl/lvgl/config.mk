71cc615c5095ec8c6e5ddac15ea5778ce2161cfa
Yersa Nordman <finwo@pm.me>
2025-07-08 22:34:58 +0200

    Lvgl fully custom mk, we're not compatible with project's

--- lvgl/lvgl/config.mk
+++ lvgl/lvgl/config.mk
@@ -1,5 +1,5 @@
 LVGL_PATH := __DIRNAME
-include __DIRNAME/lvgl.mk
-SRC+=$(CSRCS)
-SRC+=$(CXXSRCS)
+SRC += $(shell find $(LVGL_PATH)/src -type f -name '*.c')
+SRC += $(shell find $(LVGL_PATH)/src -type f -name '*.cpp')
 INCLUDES+=-I__DIRNAME/..
+override CFLAGS+=-DDLV_CONF_INCLUDE_SIMPLE
