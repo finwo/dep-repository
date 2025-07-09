8adfadde8c63ca2ffea2c450fa3ec74cde3fd5c0
finwo <finwo@pm.me>
2025-07-09 20:14:20 +0200

    tinycthread/tinycthread: lpthread on ldflags only, simplified os checks

--- tinycthread/tinycthread/config.mk
+++ tinycthread/tinycthread/config.mk
@@ -1,34 +1,6 @@
 SRC+=__DIRNAME/source/tinycthread.c
 
-ifeq ($(OS),Windows_NT)
-    # CFLAGS += -D WIN32
-    ifeq ($(PROCESSOR_ARCHITEW6432),AMD64)
-        # CFLAGS += -D AMD64
-    else
-        ifeq ($(PROCESSOR_ARCHITECTURE),AMD64)
-            # CFLAGS += -D AMD64
-        endif
-        ifeq ($(PROCESSOR_ARCHITECTURE),x86)
-            # CFLAGS += -D IA32
-        endif
-    endif
-else
-    UNAME_S := $(shell uname -s)
-    ifeq ($(UNAME_S),Linux)
-        override CFLAGS+=-lpthread
-        # CFLAGS += -D LINUX
-    endif
-    ifeq ($(UNAME_S),Darwin)
-        # CFLAGS += -D OSX
-    endif
-    UNAME_P := $(shell uname -p)
-    ifeq ($(UNAME_P),x86_64)
-        # CFLAGS += -D AMD64
-    endif
-    ifneq ($(filter %86,$(UNAME_P)),)
-        # CFLAGS += -D IA32
-    endif
-    ifneq ($(filter arm%,$(UNAME_P)),)
-        # CFLAGS += -D ARM
-    endif
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+    override LDFLAGS+=-lpthread
 endif
